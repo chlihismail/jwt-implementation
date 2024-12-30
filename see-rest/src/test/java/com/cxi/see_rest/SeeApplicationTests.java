54c097a6c0cdcb3e41924cf415a009ca9a3500df
chlihismail <ismail.public.57@gmail.com>
2024-12-31 00:00:08 +0100

    initial commit

---
+++ see-rest/src/test/java/com/cxi/see_rest/SeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxi.see_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
